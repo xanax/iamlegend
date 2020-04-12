71d7454d856ad97060cb35d632b06983a51e7c6c
Brendan Poole <b.poole@kainos.com>
2020-04-12 07:36:36 +0100

    threads

--- TileMap.gd
+++ TileMap.gd
@@ -2,25 +2,35 @@ extends TileMap
 
 var size = Vector2(100, 100)
 var Movement = preload("res://Movement.gd")
+var UpdateSemaphore = preload("res://semaphore.gd") 
+var updateSemaphore  = UpdateSemaphore.new(self, "update")
 var movements = []
+var movementIndex = 0
+var currentMovementsIndex = 0
+var lastMovementIndex = 0
 var frame
 
 func _ready():
+	movements.resize(1000000)
 	frame = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(frame == 0):
-		movements = []
-		for y in range(size.y):
-			for x in range(size.x):
-				if(self.get_cell(x, y) == -1 && self.get_cell(x, y - 1) == 1):
-					var move = Movement.new(Vector2(x,y), Vector2(0,-1))
-					movements.append(move)
-	var p = $KinematicBody2D.position/16
-	if(frame == 15):
+		updateSemaphore.increment_counter()
+		for m in range(lastMovementIndex, movementIndex):
+			movements[m].do(self)
+	frame += 1
+	if(frame > 15):
 		frame = 0
-		for m in movements:
-			m.do(self)
-	else:
-		frame += 1
+
+func update():
+	print("Updating")
+	lastMovementIndex = movementIndex
+	for y in range(size.y):
+		for x in range(size.x):
+			if(self.get_cell(x, y) == -1 && self.get_cell(x, y - 1) == 1):
+				var move = Movement.new(Vector2(x,y), Vector2(0,-1))
+				movements[movementIndex] = move
+				movementIndex+=1
+	
---
+++ semaphore.gd
@@ -0,0 +1,64 @@
+extends Object
+
+var counter = 0
+var mutex
+var semaphore
+var thread
+var exit_thread = false
+var tile_map
+var function
+
+# The thread will start here.
+func _init(tile_map : TileMap, function : String):
+	self.tile_map = tile_map
+	self.function = function
+	mutex = Mutex.new()
+	semaphore = Semaphore.new()
+	exit_thread = false
+
+	thread = Thread.new()
+	thread.start(self, "_thread_function")
+	print("started semaphore")
+
+func _thread_function(userdata):
+	while true:
+		semaphore.wait() # Wait until posted.
+
+		mutex.lock()
+		var should_exit = exit_thread # Protect with Mutex.
+		mutex.unlock()
+
+		if should_exit:
+			break
+
+		mutex.lock()
+		counter += 1 # Increment counter, protect with Mutex.
+		tile_map.call(function)
+		mutex.unlock()
+
+func increment_counter():
+	semaphore.post() # Make the thread process.
+
+func get_counter():
+	mutex.lock()
+	# Copy counter, protect with Mutex.
+	var counter_value = counter
+	mutex.unlock()
+	return counter_value
+
+# Thread must be disposed (or "joined"), for portability.
+func _exit_tree():
+	# Set exit condition to true.
+	mutex.lock()
+	exit_thread = true # Protect with Mutex.
+	mutex.unlock()
+
+	# Unblock by posting.
+	semaphore.post()
+
+	# Wait until it exits.
+	thread.wait_to_finish()
+
+	# Print the counter.
+	print("Counter is: ", counter)
+	
